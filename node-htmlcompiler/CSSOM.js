899da9865499b33cff2eb49e8bbf4215a6843e40
nojaja <free.riccia@gmail.com>
2017-06-10 17:54:34 +0900

    Update CSSOM.js

--- node-htmlcompiler/CSSOM.js
+++ node-htmlcompiler/CSSOM.js
@@ -1,14 +1,6 @@
 (function(root) {
 	'use strict';
 	var exports = {};
-	function require(path) {
-		path = path.replace(/^\.\//, '');
-		if (!exports[path]) {
-			window.console && console.warn(path, 'is missing. Most likely just a mutual inclusion.');
-		}
-		return exports;
-	}
-
 	var CSSOM = root.CSSOM = {};
 //.CommonJS
 var CSSOM = CSSOM||{};
